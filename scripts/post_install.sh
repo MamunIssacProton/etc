ebfc0c018c7258eab6a1b49fc7a6b60915aaa937
proton <mamunaiub@hotmail.com>
2022-10-06 19:42:16 +0600

    added buildspec

---
+++ scripts/post_install.sh
@@ -0,0 +1,6 @@
+set -e
+echo 'started post install'
+cd /home/ubuntu/etc
+npm install watch
+npm run
+echo 'npm run successfully'
---
+++ scripts/pre_install.sh
@@ -0,0 +1,4 @@
+set -e
+echo 'updating apt'
+sudo apt update -y
+echo 'updated apt'
